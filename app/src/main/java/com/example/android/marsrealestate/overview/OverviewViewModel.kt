25cc6cfe2824898dc3bbdd4e5deb5b34d7b1a301
sachin3618 <sachin3618singh@gmail.com>
2022-01-26 16:56:53 +0530

    Improving UX by adding ImageView which shows your internet is disconnected on top of RecylerView.

--- app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -33,19 +33,23 @@ import javax.security.auth.callback.Callback
 /**
  * The [ViewModel] that is attached to the [OverviewFragment].
  */
+enum class MarsApiStatus {LOADING, ERROR, DONE}
+
 class OverviewViewModel : ViewModel() {
 
     // The internal MutableLiveData String that stores the status of the most recent request
-    private val _status = MutableLiveData<String>()
+    private val _status = MutableLiveData<MarsApiStatus>()
 
     // The external immutable LiveData for the request status String
-    val status: LiveData<String>
+    val status: LiveData<MarsApiStatus>
         get() = _status
 
     private val _properties = MutableLiveData<List<MarsProperty>>()
 
     val properties: LiveData<List<MarsProperty>>
     get() = _properties
+
+
 
 
     private var viewmodelJob = Job()
@@ -68,14 +72,14 @@ class OverviewViewModel : ViewModel() {
         coroutineScope.launch {
 
             try {
+                _status.value = MarsApiStatus.LOADING
                 var listResult = MarsApi.retrofitService.getProperties()
-                _status.value = "Success: ${listResult.size}"
-                if(listResult.size > 0){
-                    _properties.value = listResult
-                }
+                _status.value = MarsApiStatus.DONE
+                _properties.value = listResult
 
             } catch (t: Throwable){
-                _status.value = "Failure: " + t.message
+                _status.value = MarsApiStatus.ERROR
+                _properties.value = ArrayList()
 
             }
 
